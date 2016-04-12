20b2139bd8c1888291887660e92a94b597d46fa0
孔维鹏 <yjxkwp@foxmail.com>
2016-04-12 19:23:47 +0800

    完成了大概外观框架，增加了viewpager功能，并且做了导航条

---
+++ app/src/main/java/com/example/kwp/alarm/alarmFragment.java
@@ -0,0 +1,20 @@
+package com.example.kwp.alarm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kwp on 16-4-11.
+ */
+public class alarmFragment extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.alarm_fragment,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/kwp/alarm/settingFragment.java
@@ -0,0 +1,19 @@
+package com.example.kwp.alarm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kwp on 16-4-11.
+ */
+public class settingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.alarm_fragment, container, false);
+    }
+}
